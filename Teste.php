b721406af14ce48bdd623ce8bcf91a398a76c213
Beatriz Patricio <beatrizpatriciost@gmail.com>
2023-03-22 18:17:50 -0300

    base arquivo testes unitarios

---
+++ Teste.php
@@ -0,0 +1,18 @@
+<?php
+    require_once('/IU/index.php');
+    use PHPUnit\Framework\TestCase;
+
+    /**
+     * Summary of IndexTest
+     */
+    class IndexTest extends TestCase {
+        
+    /**
+     * Teste que coloca inputs inválidos em Rua, Número e Bairro
+     */
+    public function testeEnderecoInvalido() {
+
+    }
+}
+
+?>
