d86b434504777285f19205607179b65fbe3ce861
anabias <ana.juvencio01@gmail.com>
2023-03-22 23:06:03 -0300

    Adicionando o testes bobos

--- tests/BDTest.php
+++ tests/BDTest.php
@@ -72,7 +72,63 @@
 
             $pdo = null;
         }
-        
+        //testando a localização com entradas nulas
+        public function testeLocalizacaoInvalidoNula(){
+            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
+
+            $rua = null;
+            $numero = null;
+            $bairro = null;
+            
+            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
+            $stmt = $pdo->query($comando);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->assertEquals($rua, $result['rua']);
+            $this->assertEquals($numero, $result['numero']);
+            $this->assertEquals($bairro, $result['bairro']);
+
+            $pdo = null;
+        }
+
+        //testando as entradas de localização com caracteres especiais
+        public function testeLocalizacaoCaracteresEspeciais(){
+            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
+
+            $rua = '@';
+            $numero = '%%%%';
+            $bairro = '';
+            
+            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
+            $stmt = $pdo->query($comando);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+            $this->assertEquals($rua, $result['rua']);
+            $this->assertEquals($numero, $result['numero']);
+            $this->assertEquals($bairro, $result['bairro']);
+
+            $pdo = null;
+        }
+        //testando entradas de localização com valores numericos
+        public function testeLocalizacaoNumerica(){
+            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
+
+            $rua = '1223';
+            $numero = '455445';
+            $bairro = '8999888';
+            
+            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
+            $stmt = $pdo->query($comando);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+            $this->assertEquals($rua, $result['rua']);
+            $this->assertEquals($numero, $result['numero']);
+            $this->assertEquals($bairro, $result['bairro']);
+
+            $pdo = null;
+        }
     }
 
 ?>
