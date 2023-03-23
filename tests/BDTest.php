7c52c6f657010802d0bd8ed6275647220407a738
anabias <ana.juvencio01@gmail.com>
2023-03-23 18:08:00 -0300

    retirando os testes que não são necessários

--- tests/BDTest.php
+++ tests/BDTest.php
@@ -10,7 +10,6 @@
         public function testeSelecaoTuplaComNumeroComoParametro(){
             $pdo = new PDO('mysql:host=localhost;dbname=bdbairro', 'root', '');
 
-            //bairro correto
             $bairro = 15;
 
             $comando = "SELECT * FROM Bairros WHERE nome_bairro = '$bairro'";
@@ -69,63 +68,6 @@
             $bd = bd::getInstancia();
 
             $this->assertEquals($temp, $bd->existeBairro($bairro));
-
-            $pdo = null;
-        }
-        //testando a localização com entradas nulas
-        public function testeLocalizacaoInvalidoNula(){
-            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
-
-            $rua = null;
-            $numero = null;
-            $bairro = null;
-            
-            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
-            $stmt = $pdo->query($comando);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $this->assertEquals($rua, $result['rua']);
-            $this->assertEquals($numero, $result['numero']);
-            $this->assertEquals($bairro, $result['bairro']);
-
-            $pdo = null;
-        }
-
-        //testando as entradas de localização com caracteres especiais
-        public function testeLocalizacaoCaracteresEspeciais(){
-            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
-
-            $rua = '@';
-            $numero = '%%%%';
-            $bairro = '';
-            
-            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
-            $stmt = $pdo->query($comando);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-            $this->assertEquals($rua, $result['rua']);
-            $this->assertEquals($numero, $result['numero']);
-            $this->assertEquals($bairro, $result['bairro']);
-
-            $pdo = null;
-        }
-        //testando entradas de localização com valores numericos
-        public function testeLocalizacaoNumerica(){
-            $pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
-
-            $rua = '1223';
-            $numero = '455445';
-            $bairro = '8999888';
-            
-            $comando = "SELECT * FROM localizacaocliente WHERE rua = '$rua' AND numero = '$numero' AND bairro = '$bairro'" ;
-            $stmt = $pdo->query($comando);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-            $this->assertEquals($rua, $result['rua']);
-            $this->assertEquals($numero, $result['numero']);
-            $this->assertEquals($bairro, $result['bairro']);
 
             $pdo = null;
         }
