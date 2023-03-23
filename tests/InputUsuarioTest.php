9b98025dceb210467ab985708d781252deed9590
anabias <ana.juvencio01@gmail.com>
2023-03-23 20:03:07 -0300

    inserindo novos testes

--- tests/InputUsuarioTest.php
+++ tests/InputUsuarioTest.php
@@ -15,38 +15,29 @@ class InputUsuarioTest extends TestCase{
         $this->assertEquals(false, $result);
     }
     
-
-
-
     public function testInputSpecial(){
         $bairro = $numero = $rua = "!@#$%^&*()";
         $ruaErr = $numeroErr = $bairroErr = "";
         $result = ValidaEndereco::validarEndereco($rua, $numero, $bairro, $ruaErr, $numeroErr, $bairroErr);
         $this->assertEquals(false, $result);    
     }
-
-
+    public function testInputNumerico(){
+        $bairro = $rua = "123";
+        $numero = '365';
+        $ruaErr = $numeroErr = $bairroErr = "";
+        $result = ValidaEndereco::validarEndereco($rua, $numero, $bairro, $ruaErr, $numeroErr, $bairroErr);
+        $this->assertEquals(false, $result);    
+    }
     /*
     public function testInputLongString(){
 
     }
-
+     */
     public function testInputVazia(){
-
+        $bairro = $rua = $numero = "";
+        $ruaErr = $numeroErr = $bairroErr = "";
+        $result = ValidaEndereco::validarEndereco($rua, $numero, $bairro, $ruaErr, $numeroErr, $bairroErr);
+        $this->assertEquals(false, $result);   
     }
-    */
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
